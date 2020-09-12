3ae70012e0a4c2ee570f99b3e6eef121861532c7
TOM JOSEPH <31935042+tomdxb0004@users.noreply.github.com>
2020-09-12 17:21:23 +0400

    dataset.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ TRAIN_BATH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS =10
 ACCUMULATION = 2
-BERT_PATH = "../input/bert_base_uncased"
+BERT_PATH = "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt"
 MODEL_PATH = "model.bin"
-TRAINING_FILE = "../input/imdb.csv"
+TRAINING_FILE = "..\\input\\imdb.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case = True)
